7d00b2e54cb4de5c5f1295efab110845ebaaa139
mariaeugenialucero <maria.eugenia.sud@gmail.com>
2016-06-03 18:02:41 -0400

    Initial commit

---
+++ app/src/main/java/com/example/aruuywa/CuatroActivity.java
@@ -0,0 +1,45 @@
+package com.example.aruuywa;
+
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class CuatroActivity extends AppCompatActivity {
+    int r1,r2 = 0;
+    SoundPool sp1,sp2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuatro);
+
+        sp1=new SoundPool(9, AudioManager.STREAM_MUSIC,0);
+        r1=sp1.load(getApplicationContext(), R.raw.gallina_s, 1);
+        sp2=new SoundPool(9, AudioManager.STREAM_MUSIC,0);
+        r2=sp2.load(getApplicationContext(), R.raw.gallina, 1);
+    }
+    public void animal(View v){
+        if(r1!=0)
+            sp1.play(r1 ,1 , 1, 0, 0, 1);
+    }
+
+    public void aymara(View v){
+        if(r2!=0)
+            sp2.play(r2 ,1 , 1, 0, 0, 1);
+    }
+    public void siguiente(View v){
+        Intent i = new Intent(CuatroActivity.this,CincoActivity.class);
+
+        startActivity(i);
+
+    }
+    public void anterior(View v){
+        Intent i = new Intent(CuatroActivity.this,TresActivity.class);
+
+        startActivity(i);
+
+    }
+
+}
